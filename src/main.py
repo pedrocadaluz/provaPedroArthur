ce0abc2873ac8f5d5edf8144239f33b5d0306aa1
Pedro Arthur <pedrocadaluz@gmail.com>
2024-06-14 11:21:04 -0300

    'correcao'

--- src/main.py
+++ src/main.py
@@ -19,72 +19,72 @@ def main():
     df = pd.read_excel(file)
     print (df)
 
-#fazer uma lista com os nomes
-df ='/data/nomesAtletas.xlsx'
-listaNomes = df['nome'].values.tolist()
+    #fazer uma lista com os nomes
+    df ='/data/nomesAtletas.xlsx'
+    listaNomes = df['nome'].values.tolist()
 
-#data frame
-atletas ={
-    'nome':[],
-    'idade':[],
-    'pais':[],
-    'medalha':[]
-}
-ages = []
-for nome in listaNomes:
-    age = getAge(nome)
-    ages += [age]
-    ages.append(ages)
-    print(age)
+    #data frame
+    atletas ={
+        'nome':[],
+        'idade':[],
+        'pais':[],
+        'medalha':[]
+    }
+    ages = []
+    for nome in listaNomes:
+        age = getAge(nome)
+        ages += [age]
+        ages.append(ages)
+        print(age)
 
-countrys = []
-for nome in listaNomes:
-    country = getCountry(nome)
-    countrys += [country]
-    countrys.append(countrys)
-    print(countrys)
+    countrys = []
+    for nome in listaNomes:
+        country = getCountry(nome)
+        countrys += [country]
+        countrys.append(countrys)
+        print(countrys)
 
-medals =[]
-for nome in listaNomes:
-    medal = getMedal(nome)
-    medals += [medal]
-    medals.append(medals)
-    print(medals)
-
-
-df_dicionario = pd.DataFrame(atletas)
-df['ages'] = ages
-df['countrys'] = countrys
-df['medals'] = medals
-print(df_dicionario)
-
-df_dicionario.to_excel (BASE_DIR +  '/data/listaFinal.xslx')
+    medals =[]
+    for nome in listaNomes:
+        medal = getMedal(nome)
+        medals += [medal]
+        medals.append(medals)
+        print(medals)
 
 
-#atletas 30 anos ouro
-atletas_ouro_30 = df_dicionario[(df_dicionario['ages']>30)& (df_dicionario[medal]=='Gold')]
+    df_dicionario = pd.DataFrame(atletas)
+    df['ages'] = ages
+    df['countrys'] = countrys
+    df['medals'] = medals
+    print(df_dicionario)
 
-#atletas eua
-num_atletas_eua = df_dicionario[df_dicionario['coutrys']=='United States'].shape[0]
-
-#atleta mais velho
-oldest_atleta = df_dicionario[df_dicionario['ages']==df_dicionario['ages'].max()]
-oldest_atleta = oldest_atleta['name'].tolist()
-
-#países participantes
-num_paises = df_dicionario['coutrys'].nunique()
+    df_dicionario.to_excel (BASE_DIR +  '/data/listaFinal.xslx')
 
 
+    #atletas 30 anos ouro
+    atletas_ouro_30 = df_dicionario[(df_dicionario['ages']>30)& (df_dicionario[medal]=='Gold')]
 
-#enviar e-mail yahoo automático
-usuario = os.environ.get("YAHOO_USER") 
-senha = os.environ.get("YAHOO_PASSWORD")
-destinatario = 'pedrocadaluz@outlook.com'
-assunto = 'AP2'
-mensagem = f'''
-i)atletas com mais de 30 anos que conquistaram a medalha de ouro foram {atletas_ouro_30}.
-ii)os USA ganharam {num_atletas_eua} medalhas.
-iii) O atleta mais velho  que ganhou uma medalha foi o {oldest_atleta} de {age} anos.
-iv) Nessa amostra contém {num_paises} países que ganharam medalhas.
-'''
+    #atletas eua
+    num_atletas_eua = df_dicionario[df_dicionario['coutrys']=='United States'].shape[0]
+
+    #atleta mais velho
+    oldest_atleta = df_dicionario[df_dicionario['ages']==df_dicionario['ages'].max()]
+    oldest_atleta = oldest_atleta['name'].tolist()
+
+    #países participantes
+    num_paises = df_dicionario['coutrys'].nunique()
+
+
+
+    #enviar e-mail yahoo automático
+    usuario = os.environ.get("YAHOO_USER") 
+    senha = os.environ.get("YAHOO_PASSWORD")
+    destinatario = 'pedrocadaluz@outlook.com'
+    assunto = 'AP2'
+    mensagem = f'''
+    i)atletas com mais de 30 anos que conquistaram a medalha de ouro foram {atletas_ouro_30}.
+    ii)os USA ganharam {num_atletas_eua} medalhas.
+    iii) O atleta mais velho  que ganhou uma medalha foi o {oldest_atleta} de {age} anos.
+    iv) Nessa amostra contém {num_paises} países que ganharam medalhas.
+    '''
 
